5d448820ef3fb9e5b15716ed49ac32c6eaa879e4
estefani0718 <eestefanisg@gmail.com>
2025-03-07 12:11:57 -0500

    ejercicio 15

--- Js/ejercicio15.js
+++ Js/ejercicio15.js
@@ -1,7 +1,7 @@
 // Realiza una llamada a un archivo local Muestra en formato json, luego muestre los datos
 // obtenidos en la consola.
 
-fetch('/Js/ejercicio15_json.json')//obtenemos los datos con fetch 
+fetch('/Js/json/ejercicio15_json.json')//obtenemos los datos con fetch 
     .then(resultado => {// se espera una respuesta 
         return resultado.json(); // aqui se convierte lo que hay dentro del Json tambien se devuelve una promesa 
         })
