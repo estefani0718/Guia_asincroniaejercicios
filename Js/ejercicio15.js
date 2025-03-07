e39077a691558213c4368c3afb80aa7b66850dbd
estefani0718 <eestefanisg@gmail.com>
2025-03-07 12:02:06 -0500

    ejercicio 15

---
+++ Js/ejercicio15.js
@@ -0,0 +1,13 @@
+// Realiza una llamada a un archivo local Muestra en formato json, luego muestre los datos
+// obtenidos en la consola.
+
+fetch('/Js/ejercicio15_json.json')//obtenemos los datos con fetch 
+    .then(resultado => {// se espera una respuesta 
+        return resultado.json(); // aqui se convierte lo que hay dentro del Json tambien se devuelve una promesa 
+        })
+    .then(datos=> {// necesitamos otro then obtener los datos y  mostrarlos en la consola 
+        console.log("Datos obtenidos:", datos); // se imprime los datos 
+    })
+    .catch(error => {// aqui se captuta se hay algun error 
+        console.error("Error al obtener los datos:", error);
+    });
